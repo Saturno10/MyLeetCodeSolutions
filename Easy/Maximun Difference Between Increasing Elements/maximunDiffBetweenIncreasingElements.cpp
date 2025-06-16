04b5498f7f43d7256975f58cc0dc2b871b46bffe
Álvaro Aledo Tornero <alvaroaledotornero@gmail.com>
2025-06-16 23:13:07 +0200

    add new easy problem

---
+++ Easy/Maximun Difference Between Increasing Elements/maximunDiffBetweenIncreasingElements.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int minValue = nums[0];
+        int diff = -1;
+        for (int i = 1; i < nums.size(); i++){
+            if(nums[i] > minValue){
+                diff = max(diff, nums[i] - minValue);
+            } else{
+                minValue = nums[i];
+            }
+        }
+
+        return diff;
+    }
+};
